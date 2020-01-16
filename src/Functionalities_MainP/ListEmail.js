9c012ea1a6b7d47fd13809faee0d22780a2e6313
aishadabbagh <aisha.dabbagh@gmail.com>
2020-01-16 21:27:24 +0300

    fixed icon of block and downloaded @material-ui/core

--- src/Functionalities_MainP/ListEmail.js
+++ src/Functionalities_MainP/ListEmail.js
@@ -1,4 +1,6 @@
 import React from "react";
+import BlockIcon from '@material-ui/icons/Block';
+// ./material-ui/core/SvgIcon/Block'
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Password from "../Password";
 const ListEmail = (props) =>{
@@ -8,9 +10,10 @@ const ListEmail = (props) =>{
     <li onClick={(e) => props.handleClickEvent(props.name)}>
         {props.name}
 
-        <span className="material-icons"
+        <span
+        //  className="material-icons"
             onClick={props.delete}>
-            remove_circle
+            <BlockIcon/>
         </span>
         
     </li>
