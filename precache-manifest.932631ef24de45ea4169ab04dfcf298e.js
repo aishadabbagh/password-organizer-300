f7adc8fc29ef80ed6a14babca71e6cf6345bcafe
aishadabbagh <aisha.dabbagh@gmail.com>
2020-01-16 18:17:44 +0300

    Updates

---
+++ precache-manifest.932631ef24de45ea4169ab04dfcf298e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09b39746b0268cf97561bb078f58a39",
+    "url": "/password-organizer-300/index.html"
+  },
+  {
+    "revision": "f9f5421c27777c5be3b5",
+    "url": "/password-organizer-300/static/css/main.3c92cb16.chunk.css"
+  },
+  {
+    "revision": "7e3c26bf1d3c0c890b22",
+    "url": "/password-organizer-300/static/js/2.dce37bc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/password-organizer-300/static/js/2.dce37bc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9f5421c27777c5be3b5",
+    "url": "/password-organizer-300/static/js/main.5819ef18.chunk.js"
+  },
+  {
+    "revision": "011dd23152d38b9d50b2",
+    "url": "/password-organizer-300/static/js/runtime-main.0c1d9828.js"
+  }
+]);
